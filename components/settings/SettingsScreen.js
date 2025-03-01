c41a3a1dc32e7f5281dacc2502ddc38738b0130e
KangJiW00 <rkdwldn89@naver.com>
2025-03-01 12:38:09 +0900

    [feature/settings-0001]설정 페이지 기능 추가

---
+++ components/settings/SettingsScreen.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+const SettingsScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      {/* 헤더 */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>설정</Text>
+      </View>
+
+      {/* 설정 목록을 감싸는 박스 */}
+        <View style={styles.settingsBox}>
+          <TouchableOpacity style={styles.settingItem} onPress={() => navigation.navigate('GeneralSettings')}>
+            <Text style={styles.settingText}>일반</Text>
+          </TouchableOpacity>
+          <View style={styles.separator} />
+          <TouchableOpacity style={styles.settingItem} onPress={() => navigation.navigate('NotificationSettings')}>
+            <Text style={styles.settingText}>알림</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+  },
+  backButton: {
+    marginRight: 10, // 화살표와 텍스트 간격 조정
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  settingsBox: {
+    backgroundColor: 'white',
+    borderRadius: 10, 
+    marginHorizontal: 15,
+    paddingVertical: 5,
+    shadowColor: '#000', 
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3, // 안드로이드 그림자 효과
+    borderWidth: 1,  // 검정색 선 추가
+    borderColor: 'black',  
+  },
+  settingItem: {
+    padding: 15,
+  },
+  settingText: {
+    fontSize: 16,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ddd', // 연한 회색 구분선
+    marginHorizontal: 15,
+  },
+});
+
+export default SettingsScreen;
