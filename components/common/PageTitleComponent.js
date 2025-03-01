d79a762b98dea2b12382f2a65efb7590837dcdbb
이은빈 <ei228555@gmail.com>
2025-03-01 22:26:58 +0900

    [feature/common-0001] 페이지 제목 공통 컴포넌트 수정

--- components/common/PageTitleComponent.js
+++ components/common/PageTitleComponent.js
@@ -2,12 +2,18 @@ import {StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-const PageTitleComponent = ({title}) => {
+const PageTitleComponent = ({title, backToTab, backToStack}) => {
     const navigation = useNavigation();
+
+    const back = () => {
+        if(!!backToTab) navigation.navigate("TabNavigator", {screen: `${backToTab}`});
+        else if(!!backToStack) navigation.navigate(`${backToStack}`);
+        else navigation.goBack();
+    }
 
     return (
         <View style={styles.header}>
-            <TouchableOpacity style={styles.arrow} onPress={() => navigation.goBack()}>
+            <TouchableOpacity style={styles.arrow} onPress={() => back()}>
                 <Ionicons name="arrow-back" size={24} color="black" />
             </TouchableOpacity>
             <Text style={styles.title}>{title}</Text>
@@ -20,6 +26,9 @@ const styles = StyleSheet.create({
     header: {
         flexDirection: 'row',
         justifyContent: "space-between",
+        marginTop: 30,
+        marginLeft: 20,
+        marginRight: 20,
     },
     arrow: {
         marginTop: 8,
