0bc32be72e66f5b7da27441a3286080836ea9de3
이은빈 <ei228555@gmail.com>
2025-09-14 20:52:17 +0900

    expo SDK 53 -> 54 버전업 및 기타 세팅 수정, API KEY 암호화 DB 연동 구현

--- components/home/HomeComponent.js
+++ components/home/HomeComponent.js
@@ -3,7 +3,6 @@ import ChatBotComponent from "../chatbot/ChatBotComponent";
 import {useEffect} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {useUserStore} from "../../stores/UserStore";
-import supabase from "../../supabase";
 
 const HomeComponent = () => {
     const navigation = useNavigation();
